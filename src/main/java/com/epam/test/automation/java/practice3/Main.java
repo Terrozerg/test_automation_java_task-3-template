fecb527cb4b79e3fa2cf10aa68e073150b090d5d
Stanislav <Terrozerg@gmail.com>
2021-03-29 01:21:16 +0300

    1

--- src/main/java/com/epam/test/automation/java/practice3/Main.java
+++ src/main/java/com/epam/test/automation/java/practice3/Main.java
@@ -2,14 +2,28 @@ package com.epam.test.automation.java.practice3;
 
 public class Main {
 
+    private Main(){}
+
     /**
      * <summary>
      * Implement code according to description of task 1.
      * </summary>
      */
     public static int[] task1(int[] array) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        if(array==null){
+            throw new IllegalArgumentException("Cant pass null array.");
+        }
+        int arrayLength = array.length;
+
+        for (int i = 0, j=arrayLength-1; i < j; i++, j--) {
+            if((array[i]&1)==0 && (array[j]&1)==0){
+                int swap = array[i];
+                array[i] = array[j];
+                array[j] = swap;
+            }
+        }
+
+        return array;
     }
 
     /**
@@ -18,8 +32,25 @@ public class Main {
      * </summary>
      */
     public static int task2(int[] array) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        if(array==null){
+            throw new IllegalArgumentException("Cant pass null array.");
+        }
+
+        int maxPos = 0;
+        int firstMaximumPos = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if(array[i]>=array[maxPos]){
+                //get position of first maximum
+                if(firstMaximumPos==0 && array[i]!=array[maxPos]){
+                    firstMaximumPos=i;
+                }
+
+                maxPos = i;
+            }
+        }
+
+        return maxPos-firstMaximumPos;
     }
 
     /**
@@ -28,8 +59,28 @@ public class Main {
      * </summary>
      */
     public static int[][] task3(int[][] matrix) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
-    }
+        if(matrix==null){
+            throw new IllegalArgumentException("Cant pass null matrix.");
+        }
 
+        if(matrix.length != matrix[0].length){
+            throw new IllegalArgumentException("Cant pass non-square matrix.");
+        }
+
+        int diagonal = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < diagonal; j++) {
+                matrix[i][j] = 0;
+            }
+
+            for (int j = diagonal+1; j < matrix.length; j++) {
+                matrix[i][j] = 1;
+            }
+
+            diagonal++;
+        }
+
+        return matrix;
+    }
 }
--- src/test/java/com/epam/test/automation/java/practice3/MainTest.java
+++ src/test/java/com/epam/test/automation/java/practice3/MainTest.java
@@ -1,5 +1,90 @@
 package com.epam.test.automation.java.practice3;
 
-public class MainTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class MainTest {
+    @Test
+    public void task1PassTest(){
+        Assert.assertEquals(new int[]{4,5,3,10},
+                Main.task1(new int[]{10, 5, 3, 4}));
+        Assert.assertEquals(new int[]{4,5,-3,-10},
+                Main.task1(new int[]{-10, 5, -3, 4}));
+    }
+
+    @Test
+    public void task1BorderTest(){
+        int maxEvenNumber = Integer.MAX_VALUE-1;
+        Assert.assertEquals(new int[]{-88,46,3,Integer.MIN_VALUE,maxEvenNumber},
+                Main.task1(new int[]{maxEvenNumber, Integer.MIN_VALUE, 3, 46, -88}));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void task1NullTest(){
+        Main.task1(null);
+    }
+
+    @Test
+    public void task2PositiveIntegersArrayTest(){
+        Assert.assertEquals(2,Main.task2(new int[]{4, 100, 55, 100, 4}));
+    }
+
+    @Test
+    public void task2NegativeIntegersArrayTest(){
+        Assert.assertEquals(3,Main.task2(new int[]{-4, -100, 55, -100, 4, 55, 0}));
+    }
+
+    @Test
+    public void task2AllNegativeIntegersArrayTest(){
+        Assert.assertEquals(4,Main.task2(new int[]{-4, -100, -55, -100, -4, -55}));
+    }
+
+    @Test
+    public void task2OnlyOneMaxTest(){
+        Assert.assertEquals(0,Main.task2(new int[]{0, 100, 55, 6, 4}));
+    }
+
+    @Test
+    public void task2AllItemsEqualMaxTest(){
+        Assert.assertEquals(5,Main.task2(new int[]{4, 4, 4, 4, 4, 4}));
+    }
+
+    @Test
+    public void task2BorderTest(){
+        Assert.assertEquals(3,
+                Main.task2(new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE, 4, 40, Integer.MAX_VALUE, -66}));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void task2NullTest(){
+        Main.task2(null);
+    }
+
+    @Test
+    public void task3PassTest(){
+        int[][] input = new int[][]{{2,4,-3,3},{2,-1,1,1},{5,7,-8,5},{3,7,9,1}};
+        int[][] output = new int[][]{{2,1,1,1},{0,-1,1,1},{0,0,-8,1},{0,0,0,1}};
+        Assert.assertEquals(output, Main.task3(input));
+    }
+
+    @Test
+    public void task3BorderTest(){
+        int[][] input = new int[][]{{2,Integer.MAX_VALUE,-3,3},{2,-1,1,1},{5,7,Integer.MIN_VALUE,5},{3,7,9,1}};
+        int[][] output = new int[][]{{2,1,1,1},{0,-1,1,1},{0,0,Integer.MIN_VALUE,1},{0,0,0,1}};
+        Assert.assertEquals(output, Main.task3(input));
+    }
+
+    @Test(
+            expectedExceptions = IllegalArgumentException.class,
+            expectedExceptionsMessageRegExp = "Cant pass null matrix.")
+    public void task3MatrixNullTest(){
+        Main.task3(null);
+    }
+
+    @Test(
+            expectedExceptions = IllegalArgumentException.class,
+            expectedExceptionsMessageRegExp = "Cant pass non-square matrix.")
+    public void task3MatrixNonSquareTest(){
+        Main.task3(new int[][]{{2,-4,3,3},{2,-1,1,1},{5,7,8,5}});
+    }
 }
